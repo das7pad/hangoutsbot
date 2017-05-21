e40f5c2101c08422d06359dd59d91a470a9d2b46
das7pad <das7pad@outlook.com>
2017-05-21 06:42:42 +0200

    minor documentation, skip the duplicate chatid in logging
    chat_id and gaia_id are always the same

--- hangupsbot/event.py
+++ hangupsbot/event.py
@@ -1,7 +1,12 @@
+"""Hangups Events to Hangupsbot event mapping
+
+the following events provide the properties that are in general needed to
+identify a user of a message, the message content and the conversation
+"""
+
 import logging
 
 import hangups
-
 
 logger = logging.getLogger(__name__)
 
@@ -37,7 +42,8 @@ class TypingEvent(StatusEvent):
     def __init__(self, bot, state_update_event):
         super().__init__(bot, state_update_event)
 
-        self.conv_event = hangups.parsers.parse_typing_status_message(state_update_event)
+        self.conv_event = hangups.parsers.parse_typing_status_message(
+            state_update_event)
 
         self.user_id = state_update_event.sender_id
         self.timestamp = state_update_event.timestamp
@@ -53,7 +59,8 @@ class WatermarkEvent(StatusEvent):
     def __init__(self, bot, state_update_event):
         super().__init__(bot, state_update_event)
 
-        self.conv_event =  hangups.parsers.parse_watermark_notification(state_update_event)
+        self.conv_event =  hangups.parsers.parse_watermark_notification(
+            state_update_event)
 
         self.user_id = state_update_event.sender_id
         self.timestamp = state_update_event.latest_read_timestamp
@@ -76,14 +83,22 @@ class ConversationEvent(GenericEvent):
         self.user_id = conv_event.user_id
         self.user = self.conv.get_user(self.user_id)
         self.timestamp = conv_event.timestamp
-        self.text = conv_event.text.strip() if isinstance(conv_event, hangups.ChatMessageEvent) else ''
+        self.text = (conv_event.text.strip()
+                     if isinstance(conv_event, hangups.ChatMessageEvent)
+                     else '')
 
         self.log()
 
 
     def log(self):
-        if logger.isEnabledFor(self.emit_log):
-            logger.log(self.emit_log, 'eid/dt: {}/{}'.format(self.event_id, self.timestamp.astimezone(tz=None).strftime('%Y-%m-%d %H:%M:%S')))
-            logger.log(self.emit_log, 'cid/cn: {}/{}'.format(self.conv_id, self.bot.conversations.get_name(self.conv)))
-            logger.log(self.emit_log, 'c/g/un: {}/{}/{}'.format(self.user_id.chat_id, self.user_id.gaia_id, self.user.full_name))
-            logger.log(self.emit_log, 'len/tx: {}/{}'.format(len(self.text), self.text))
+        """log meta of the event"""
+        if not logger.isEnabledFor(self.emit_log):
+            return
+
+        logger.log(self.emit_log, 'eid/dt: %s/%s', self.event_id,
+                   self.timestamp.astimezone().strftime('%Y-%m-%d %H:%M:%S'))
+        logger.log(self.emit_log, 'cid/cn: %s/%s',
+                   self.conv_id, self.bot.conversations.get_name(self.conv))
+        logger.log(self.emit_log, '  c/un: %s/%s',
+                   self.user_id.chat_id, self.user.full_name)
+        logger.log(self.emit_log, 'len/tx: %s/%s', len(self.text), self.text)
