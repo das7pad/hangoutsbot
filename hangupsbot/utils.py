ed04bd56553264d439f3ae8d0229665c15c9f750
das7pad <das7pad@outlook.com>
2017-05-25 13:38:49 +0200

    remove legacy code, add style, split imports
    remove the not needed hangups_shim import as hangups

--- hangupsbot/utils.py
+++ hangupsbot/utils.py
@@ -1,7 +1,8 @@
 # coding: utf-8
-import importlib, logging, unicodedata
-
-import hangups_shim as hangups
+# pylint: disable=unused-import
+import importlib
+import logging
+import unicodedata
 
 from parsers import simple_parse_to_segments, segment_to_html
 
@@ -10,35 +11,16 @@ from permamem import name_from_hangups_conversation
 
 logger = logging.getLogger(__name__)
 
-
-def text_to_segments(text):
-    """Create list of message segments from text"""
-    # Replace two consecutive spaces with space and non-breakable space,
-    # then split text to lines
-    lines = text.replace('  ', ' \xa0').splitlines()
-    if not lines:
-        return []
-
-    # Generate line segments
-    segments = []
-    for line in lines[:-1]:
-        if line:
-            segments.append(hangups.ChatMessageSegment(line))
-        segments.append(hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK))
-    if lines[-1]:
-        segments.append(hangups.ChatMessageSegment(lines[-1]))
-
-    return segments
-
-
 def remove_accents(text):
     """remove accents from unicode text, allows east asian languages through"""
-    return ''.join(c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')
+    return ''.join(c for c in unicodedata.normalize('NFD', text)
+                   if unicodedata.category(c) != 'Mn')
 
 
 def unicode_to_ascii(text):
     """Transliterate unicode characters to ASCII"""
-    return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode()
+    return unicodedata.normalize('NFKD',
+                                 text).encode('ascii', 'ignore').decode()
 
 
 def class_from_name(module_name, class_name):
