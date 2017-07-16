5c1dbd1571863a6c3f3d3b5b5802b004c9895ae9
das7pad <das7pad@outlook.com>
2017-07-16 22:20:25 +0200

    add documentation and suppress pylint warning

--- hangupsbot/exceptions.py
+++ hangupsbot/exceptions.py
@@ -1,13 +1,19 @@
+"""Exceptions used for the HangupsBot core"""
+
 class SuppressHandler(Exception):
-    pass
+    """suppress a single handler"""
 
 class SuppressAllHandlers(Exception):
-    pass
+    """suppress all handler on a single pluggable level"""
 
 class SuppressEventHandling(Exception):
-    pass
+    """suppress any further handling of an event"""
+
 
 class HangupsBotExceptions:
+    """Exceptions used for event handler suppressing"""
+    # pylint: disable=too-few-public-methods
+
     SuppressHandler = SuppressHandler
     SuppressAllHandlers = SuppressAllHandlers
     SuppressEventHandling = SuppressEventHandling
