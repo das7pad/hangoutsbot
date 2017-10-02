45405a42466dea2d3bc1c4cc7ad38819f1f0c949
das7pad <das7pad@outlook.com>
2017-10-02 05:18:35 +0200

    [plugins] import image-upload methods from the image package

--- hangupsbot/plugins/image/image_links.py
+++ hangupsbot/plugins/image/image_links.py
@@ -1,9 +1,10 @@
 """
 Identify images, upload them to google plus, post in hangouts
 """
-import asyncio, logging, re
+import logging
 
 import plugins
+from plugins.image import image_validate_and_upload_single
 
 
 logger = logging.getLogger(__name__)
@@ -21,83 +22,7 @@ async def _watch_image_link(bot, event, command):
     text = event.text
 
     if text.startswith(('https://', 'http://', '//')):
-        try:
-            image_id = await bot.call_shared('image_validate_and_upload_single', text)
-        except KeyError:
-            logger.warning("image plugin not loaded - using in-built fallback")
-            image_id = await image_validate_and_upload_single(text, bot)
+        image_id = await image_validate_and_upload_single(text)
 
         if image_id:
             await bot.coro_send_message(event.conv.id_, None, image_id=image_id)
-
-
-"""FALLBACK CODE FOR IMAGE LINK VALIDATION AND UPLOAD
-* CODE IS DEPRECATED - DO NOT CHANGE OR ALTER
-* CODE MAY BE REMOVED AT ANY TIME
-* FOR FUTURE-PROOFING, INCLUDE [image] PLUGIN IN YOUR CONFIG.JSON
-"""
-
-import aiohttp, io, os, re
-
-def image_validate_link(image_uri, reject_googleusercontent=True):
-    """
-    validate and possibly mangle supplied image link
-    returns False, if not an image link
-            <string image uri>
-    """
-
-    if " " in image_uri:
-        """immediately reject anything with non url-encoded spaces (%20)"""
-        return False
-
-    probable_image_link = False
-
-    image_uri_lower = image_uri.lower()
-
-    if re.match(r"^(https?://)?([a-z0-9.]*?\.)?imgur.com/", image_uri_lower, re.IGNORECASE):
-        """imgur links can be supplied with/without protocol and extension"""
-        probable_image_link = True
-
-    elif image_uri_lower.startswith(("http://", "https://", "//")) and image_uri_lower.endswith((".png", ".gif", ".gifv", ".jpg", ".jpeg")):
-        """other image links must have protocol and end with valid extension"""
-        probable_image_link = True
-
-    if probable_image_link and reject_googleusercontent and ".googleusercontent." in image_uri_lower:
-        """reject links posted by google to prevent endless attachment loop"""
-        logger.debug("rejected link {} with googleusercontent".format(image_uri))
-        return False
-
-    if probable_image_link:
-
-        """special handler for imgur links"""
-        if "imgur.com" in image_uri:
-            if not image_uri.endswith((".jpg", ".gif", "gifv", "webm", "png")):
-                image_uri = image_uri + ".gif"
-            image_uri = "https://i.imgur.com/" + os.path.basename(image_uri)
-
-            """imgur wraps animations in player, force the actual image resource"""
-            image_uri = image_uri.replace(".webm",".gif")
-            image_uri = image_uri.replace(".gifv",".gif")
-
-        logger.debug('{} seems to be a valid image link'.format(image_uri))
-
-        return image_uri
-
-    return False
-
-async def image_upload_single(image_uri, bot):
-    logger.info("getting {}".format(image_uri))
-    filename = os.path.basename(image_uri)
-    async with aiohttp.ClientSession() as session:
-        async with session.request('get', image_uri) as res:
-            raw = await res.read()
-    image_data = io.BytesIO(raw)
-    image_id = await bot._client.upload_image(image_data, filename=filename)
-    return image_id
-
-async def image_validate_and_upload_single(text, bot, reject_googleusercontent=True):
-    image_id = False
-    image_link = image_validate_link(text, reject_googleusercontent=reject_googleusercontent)
-    if image_link:
-        image_id = await image_upload_single(image_link, bot)
-    return image_id
