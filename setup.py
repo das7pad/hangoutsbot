1806eaa883877f3a8332214f26d7b5de0bbad98e
Unknown <das7pad@outlook.com>
2017-10-31 02:43:24 +0100

    [pip] use a proxy file to comply with multiple egg-formats
    `pip-compile` wants the egg-format `#egg=package==version` and only allows `--editable` url targets
    
    `pip setup.py` wants the egg-format `#egg=package==package-version`

--- setup.py
+++ setup.py
@@ -29,29 +29,21 @@ with open('requirements.txt', 'r') as file:
         else:
             INSTALL_REQUIRES.append(line)
 
-# allow packages to overwrite requirements:
-# e.g. `pkg==0.1` from `requirements.txt` with own update `pkg==0.2.1`:
+# parse packages from urls:
+# e.g. `pkg==0.2.1` from
 #      `git+https://github.com/username/reponame@identifier#egg=pkg==pkg-0.2.1`
-DEPENDENCY_LINK_EGGS = []
 for line in DEPENDENCY_LINKS:
+    raw = None
     for item in line.split('#'):
         if item[:4] == 'egg=':
-            # add `pkg==pkg-0.2.1`
-            DEPENDENCY_LINK_EGGS.append(item[4:])
-for dependency in INSTALL_REQUIRES.copy():
-    # find package `pkg`
-    package = dependency.split('==')[0]
-    extra_dependency = None
-    for extra_dependency in DEPENDENCY_LINK_EGGS:
-        if extra_dependency.startswith(package):
+            raw = item[4:]
             break
     else:
         continue
-    INSTALL_REQUIRES.remove(dependency) # drop the old version tag
 
     # parse `pkg==pkg-0.2.1` to `pkg==0.2.1` and add it into the requirements
-    raw_extra_dependency_version = extra_dependency.split('==', 1)[-1]
-    INSTALL_REQUIRES.append(raw_extra_dependency_version.replace('-', '==', 1))
+    dependency_locked = raw.split('==', 1)[-1].replace('-', '==', 1)
+    INSTALL_REQUIRES.append(dependency_locked)
 
 
 PACKAGES = [path[:-12].replace('/', '.')
