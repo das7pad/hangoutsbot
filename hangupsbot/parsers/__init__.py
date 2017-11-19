04058b08d0c55ecaa966d6c80741bb2e4b7122e7
das7pad <das7pad@outlook.com>
2017-11-19 20:12:48 +0100

    [parsers] drop the `kludgy_html_parser` module

--- hangupsbot/parsers/__init__.py
+++ hangupsbot/parsers/__init__.py
@@ -5,7 +5,7 @@ more parsers and parser utility functions can be imported here
 #pylint: disable=unused-import
 import hangups
 
-from hangupsbot.parsers.kludgy_html_parser import segment_to_html
+from hangupsbot.sync.parser import get_formatted
 
 def simple_parse_to_segments(formatted_text):
     """parse text to hangups.ChatMessageSegements
@@ -17,3 +17,14 @@ def simple_parse_to_segments(formatted_text):
         a list of hangups.ChatMessageSegment
     """
     return hangups.ChatMessageSegment.from_str(formatted_text)
+
+def segment_to_html(segment):
+    """deprecated: parse the segment content to html
+
+    Args:
+        segment (hangups.ChatMessageSegment): message segment
+
+    Returns:
+        str: html formatted message segment
+    """
+    return get_formatted([segment,], 'html')
