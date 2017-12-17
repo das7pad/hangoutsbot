f699cf193ea5f5269d6ec86155ff709eea3f3a25
das7pad <das7pad@outlook.com>
2017-12-17 05:47:55 +0100

    [tests] drop duplicate logging

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,5 @@
 """setup the test-env"""
 
-import logging
-import sys
-
 from . import fixtures
 from .fixtures import (
     bot,
@@ -11,22 +8,3 @@ from .fixtures import (
 )
 
 __all__ = fixtures.__all__
-
-LOGGING_FORMAT = (
-    "%(relativeCreated)d "
-    "%(levelname)s "
-    "%(name)s[%(filename)s::%(funcName)s@L%(lineno)d]"
-    ": %(message)s")
-
-def pytest_report_header(config):
-    verbose = config.getvalue('verbose')
-    if verbose < 1:
-        return 'Disabled logging'
-    level = logging.DEBUG if verbose > 2 else logging.INFO
-    logging.basicConfig(
-        stream=sys.stdout,
-        level=level,
-        format=LOGGING_FORMAT,
-    )
-    out = 'Logging at level %s' % ('DEBUG' if verbose > 1 else 'INFO')
-    return out
