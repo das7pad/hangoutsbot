e9797c2d160b93ab6974718e38a12169d0fc0cf7
Jakob Ackermann <das7pad@outlook.com>
2019-01-16 14:30:35 +0100

    [tests.plugins.default] config: add tests

--- tests/test_plugins/test_default.py
+++ tests/test_plugins/test_default.py
@@ -8,6 +8,7 @@
 
 
 import asyncio
+import json
 
 import pytest
 
@@ -105,3 +106,155 @@ async def test_whoami(bot, event):
     result = await run_cmd(bot, event)
     assert label in result
     assert event.CHAT_ID in result
+
+
+@pytest.mark.asyncio
+async def test_config(bot, event):
+    test_data = {
+        'list_empty': [],
+        'list_one': ['one'],
+        'outer': {
+            'inner': 'VALUE'
+        }
+    }
+
+    bot.config.set_by_path(['test'], test_data)
+
+    # get
+    event = event.for_command('config', 'here', 'get', 'test')
+    result = await run_cmd(bot, event)
+    actual = json.loads(result.split('\n', 1)[-1])
+    assert actual == test_data
+
+    event = event.for_command('config', 'here', 'get', 'test', 'list_empty')
+    result = await run_cmd(bot, event)
+    actual = json.loads(result.split('\n', 1)[-1])
+    assert actual == test_data['list_empty']
+
+    event = event.for_command('config', 'here', 'get', 'test', 'outer')
+    result = await run_cmd(bot, event)
+    actual = json.loads(result.split('\n', 1)[-1])
+    assert actual == test_data['outer']
+    # /get
+
+    # set
+    value = 'VALUE'
+    path = ['test', 'new']
+    event = event.for_command(
+        'config', 'here', 'set', *path, json.dumps(value))
+    await run_cmd(bot, event)
+    assert bot.config.get_by_path(path) == value
+
+    value = 'OTHER_VALUE'
+    path = ['test', 'outer', 'inner']
+    event = event.for_command(
+        'config', 'here', 'set', *path, json.dumps(value))
+    await run_cmd(bot, event)
+    assert bot.config.get_by_path(path) == value
+
+    event = event.for_command('config', 'here', 'set')
+    with pytest.raises(commands.Help) as exec_info:
+        await run_cmd(bot, event)
+    assert 'missing path' in repr(exec_info.value).lower()
+
+    event = event.for_command('config', 'here', 'set', 'test')
+    with pytest.raises(commands.Help) as exec_info:
+        await run_cmd(bot, event)
+    assert 'missing value' in repr(exec_info.value).lower()
+
+    event = event.for_command(
+        'config', 'here', 'set', 'test', '[bad', 'json', 'value]')
+    result = await run_cmd(bot, event)
+    assert 'invalid json' in result.lower()
+
+    event = event.for_command(
+        'config', 'here', 'set', 'test', 'list_empty', 'inner', '[]')
+    result = await run_cmd(bot, event)
+    assert 'non-dict' in result.lower()
+    # /set
+
+    # append
+    value = 'ITEM'
+    path = ['test', 'list_empty']
+    event = event.for_command(
+        'config', 'here', 'append', *path, json.dumps(value))
+    await run_cmd(bot, event)
+    assert bot.config.get_by_path(path) == [value]
+    bot.config.set_by_path(path, [])
+
+    path = ['test', 'outer']
+    event = event.for_command(
+        'config', 'here', 'append', *path, json.dumps(value))
+    result = await run_cmd(bot, event)
+    assert 'non-list' in result.lower()
+
+    event = event.for_command(
+        'config', 'here', 'append', 'test', 'NEW_KEY', '"one"')
+    result = await run_cmd(bot, event)
+    assert 'not exist' in result.lower()
+    # /append
+
+    # remove
+    path = ['test', 'outer']
+    event = event.for_command(
+        'config', 'here', 'remove', *path, json.dumps('SOME_VALUE'))
+    result = await run_cmd(bot, event)
+    assert 'non-list' in result.lower()
+
+    path = ['test', 'list_one']
+    event = event.for_command(
+        'config', 'here', 'remove', *path, json.dumps('one'))
+    await run_cmd(bot, event)
+    assert bot.config.get_by_path(path) == []
+
+    event = event.for_command(
+        'config', 'here', 'remove', 'test', 'NEW_KEY', '"one"')
+    result = await run_cmd(bot, event)
+    assert 'not exist' in result.lower()
+    # /remove
+
+    # test
+    event = event.for_command(
+        'config', 'here', 'test', 'non-wrapped')
+    result = await run_cmd(bot, event)
+    assert 'invalid json' in result.lower()
+
+    value = '"wrapped"'
+    event = event.for_command(
+        'config', 'here', 'test', value)
+    result = await run_cmd(bot, event)
+    assert result == value
+
+    value = '[]'
+    event = event.for_command(
+        'config', 'here', 'test', value)
+    result = await run_cmd(bot, event)
+    assert result == value
+
+    event = event.for_command(
+        'config', 'here', 'test', '[[]')
+    result = await run_cmd(bot, event)
+    assert 'invalid json' in result.lower()
+
+    event = event.for_command(
+        'config', 'here', 'test', '{[]}')
+    result = await run_cmd(bot, event)
+    assert 'invalid json' in result.lower()
+
+    event = event.for_command(
+        'config', 'here', 'test', "{'test': 'value'}")
+    result = await run_cmd(bot, event)
+    assert 'invalid json' in result.lower()
+
+    value = '{"test": "value"}'
+    event = event.for_command(
+        'config', 'here', 'test', value)
+    result = await run_cmd(bot, event)
+    assert json.loads(result) == json.loads(value)
+
+    value = '{\n  "test": "value"\n}'
+    event = event.for_command(
+        'config', 'here', 'test', value)
+    result = await run_cmd(bot, event)
+    assert result == value
+    # /test
